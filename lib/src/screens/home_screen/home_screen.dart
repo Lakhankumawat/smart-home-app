39b14c5e6088c94e20f29793a208f8fa0e37b493
Ashish Chaubey <ashishchaubey866@gmail.com>
2022-03-05 22:30:20 +0530

    add profile screen

--- lib/src/screens/home_screen/home_screen.dart
+++ lib/src/screens/home_screen/home_screen.dart
@@ -1,10 +1,11 @@
 import 'package:domus/config/size_config.dart';
 import 'package:domus/provider/base_view.dart';
+import 'package:domus/src/screens/profile/landing.dart';
 import 'package:domus/src/widgets/custom_bottom_nav_bar.dart';
 import 'package:domus/view/home_screen_view_model.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
 import 'components/body.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -40,17 +41,27 @@ class HomeScreen extends StatelessWidget {
                         'Hi, Lex',
                         style: Theme.of(context).textTheme.headline1,
                       ),
-                      Container(
-                        width: 50,
-                        height: 50,
-                        decoration: BoxDecoration(
-                          color: Color(0xffdadada),
-                          borderRadius:
-                              BorderRadius.all(Radius.elliptical(45, 45)),
-                        ),
-                        child: Icon(
-                          FontAwesomeIcons.solidUser,
-                          color: Colors.amber,
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (_) => ProfileLandingScreen(),
+                            ),
+                          );
+                        },
+                        child: Container(
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            color: Color(0xffdadada),
+                            borderRadius:
+                                BorderRadius.all(Radius.elliptical(45, 45)),
+                          ),
+                          child: Icon(
+                            FontAwesomeIcons.solidUser,
+                            color: Colors.amber,
+                          ),
                         ),
                       ),
                     ],
---
+++ lib/src/screens/profile/landing.dart
@@ -0,0 +1,184 @@
+import 'package:domus/config/size_config.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class ProfileLandingScreen extends StatefulWidget {
+  const ProfileLandingScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ProfileLandingScreen> createState() => _ProfileState();
+}
+
+class _ProfileState extends State<ProfileLandingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [
+            SizedBox(
+              height: getProportionateScreenHeight(50),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: getProportionateScreenWidth(36),
+              ),
+              child: Row(
+                children: [
+                  Text(
+                    "ProfileLandingScreen",
+                    style: TextStyle(
+                      fontSize: 36,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  Spacer(),
+                  SvgPicture.asset(
+                    'assets/icons/svg/profile.svg',
+                    height: getProportionateScreenHeight(28),
+                    width: getProportionateScreenWidth(28),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: getProportionateScreenHeight(24),
+            ),
+            Stack(
+              alignment: Alignment.bottomRight,
+              children: [
+                Container(
+                  height: getProportionateScreenHeight(80),
+                  width: getProportionateScreenWidth(80),
+                  decoration: BoxDecoration(
+                    color: Colors.black38,
+                    borderRadius: BorderRadius.circular(
+                      100,
+                    ),
+                  ),
+                ),
+                SvgPicture.asset(
+                  'assets/icons/svg/edit_pen.svg',
+                ),
+              ],
+            ),
+            SizedBox(
+              height: getProportionateScreenHeight(6),
+            ),
+            Text(
+              "Ashish",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w300,
+              ),
+            ),
+            Text(
+              "Smart User",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w300,
+              ),
+            ),
+            SizedBox(
+              height: getProportionateScreenHeight(50),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                left: getProportionateScreenHeight(50),
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    child: ProfileListItems(
+                      iconPath: 'assets/icons/svg/lock.svg',
+                      itemName: "Privacy",
+                      cta: () {},
+                    ),
+                  ),
+                  SizedBox(
+                    height: getProportionateScreenHeight(35),
+                  ),
+                  Container(
+                    child: ProfileListItems(
+                      iconPath: 'assets/icons/svg/favourite.svg',
+                      itemName: "Favourites",
+                      cta: () {},
+                    ),
+                  ),
+                  SizedBox(
+                    height: getProportionateScreenHeight(35),
+                  ),
+                  Container(
+                    child: ProfileListItems(
+                      iconPath: 'assets/icons/svg/help.svg',
+                      itemName: "Help Center",
+                      cta: () {},
+                    ),
+                  ),
+                  SizedBox(
+                    height: getProportionateScreenHeight(35),
+                  ),
+                  Container(
+                    child: ProfileListItems(
+                      iconPath: 'assets/icons/svg/gift.svg',
+                      itemName: "Invite a friend",
+                      cta: () {},
+                    ),
+                  ),
+                  SizedBox(
+                    height: getProportionateScreenHeight(35),
+                  ),
+                  Container(
+                    child: ProfileListItems(
+                      iconPath: 'assets/icons/svg/sign_out.svg',
+                      itemName: "Sign out",
+                      cta: () {},
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ProfileListItems extends StatelessWidget {
+  final String iconPath;
+  final String itemName;
+  final Function cta;
+  const ProfileListItems({
+    Key? key,
+    required this.iconPath,
+    required this.itemName,
+    required this.cta,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => cta,
+      child: Container(
+        child: Row(
+          children: [
+            SvgPicture.asset(
+              iconPath,
+            ),
+            SizedBox(
+              width: getProportionateScreenWidth(25),
+            ),
+            Text(
+              itemName,
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w300,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
