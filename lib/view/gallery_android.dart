c79ed7e0b9365c2fc6d4043ce6179b9ac4eefea6
Seif Ashraf <seifibrahim32@GMail.com>
2022-05-13 14:16:46 +0200

    Delete lib directory

--- lib/view/gallery_android.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/cupertino.dart';
-import 'package:image_picker/image_picker.dart';
-
-Future<XFile?> openGallery(BuildContext context) async{
-  final pickedFile = await ImagePicker()
-      .pickImage(source: ImageSource.gallery);
-
-  return pickedFile;
-}
