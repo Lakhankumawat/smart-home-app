d42773e13f2db462600fc4de5670b7c67330b538
Seif Ashraf <seifibrahim32@gmail.com>
2022-05-16 20:36:53 +0200

    Firebase mods

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:domus/service/navigation_service.dart';
 import 'package:domus/src/logs/login_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:get_it/get_it.dart';
 
 void main() async {
 
