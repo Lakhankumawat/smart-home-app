7a731ed7bc1e2a354623db4bb1cf15e4162b99a4
Seif Ashraf <seifibrahim32@gmail.com>
2022-05-29 16:43:15 +0200

    Removed Expanded widget

--- lib/src/screens/home_screen/components/body.dart
+++ lib/src/screens/home_screen/components/body.dart
@@ -31,114 +31,112 @@ class _BodyState extends State<Body> {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Expanded(
-        child: ListView(
-          physics: BouncingScrollPhysics(),
-          shrinkWrap: true,
-          children: [
-            Container(
-              padding: EdgeInsets.symmetric(
-                horizontal: getProportionateScreenWidth(7),
-                vertical: getProportionateScreenHeight(7),
-              ),
-              decoration: BoxDecoration(
-                color: Color(0xFFF2F2F2),
-              ),
-              child: Column(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.all(getProportionateScreenHeight(0)),
-                    child: WeatherContainer(context: context, model: widget.model!),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(getProportionateScreenHeight(5)),
-                    child: SavingsContainer(context: context, model: widget.model!),
-                  ),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Padding(
-                          padding: EdgeInsets.all(getProportionateScreenHeight(5)),
-                          child: DarkContainer(
-                            itsOn: widget.model!.isLightOn ,
-                            switchButton: () {
-                              widget.model!.lightSwitch();
-                              widget.model!.isLightOn?
-                              model!.getValue(value :'on'):
-                              model!.getValue(value: 'off');
-                              Navigator.of(context).pushNamed(SmartLight.routeName);
-                            },
-                            onTap: widget.model!.lightSwitch,
-                            iconAsset: 'assets/icons/svg/light.svg',
-                            device: 'Lightening',
-                            deviceCount: '1 lamp',
-                          ),
-                        ),
-                      ),
-                      Expanded(
-                        child: Padding(
-                          padding: EdgeInsets.all(getProportionateScreenHeight(5)),
-                          child: DarkContainer(
-                            itsOn: widget.model!.isACON,
-                            switchButton: widget.model!.acSwitch,
-                            onTap: () {
-                              Navigator.of(context).pushNamed(SmartAC.routeName);
-                            },
-                            iconAsset: 'assets/icons/svg/ac.svg',
-                            device: 'AC',
-                            deviceCount: '4 devices',
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(getProportionateScreenHeight(5)),
-                    child: MusicWidget(context),
-                  ),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Padding(
-                          padding: EdgeInsets.all(getProportionateScreenHeight(5)),
-                          child: DarkContainer(
-                            itsOn: widget.model!.isSpeakerON,
-                            switchButton: widget.model!.speakerSwitch,
-                            onTap: () {
-                              Navigator.of(context).pushNamed(SmartSpeaker.routeName);
-                            },
-                            iconAsset: 'assets/icons/svg/speaker.svg',
-                            device: 'Speaker',
-                            deviceCount: '1 device',
-                          ),
-                        ),
-                      ),
-                      Expanded(
-                        child: Padding(
-                          padding: EdgeInsets.all(getProportionateScreenHeight(5)),
-                          child: DarkContainer(
-                            itsOn: widget.model!.isFanON,
-                            switchButton: widget.model!.fanSwitch,
-                            onTap: () {
-                              Navigator.of(context).pushNamed(SmartAC.routeName);
-                            },
-                            iconAsset: 'assets/icons/svg/fan.svg',
-                            device: 'Fan',
-                            deviceCount: '2 devices',
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(getProportionateScreenHeight(8)),
-                    child: AddNewDevice(context),
-                  ),
-                ],
-              ),
+      child: ListView(
+        physics: BouncingScrollPhysics(),
+        shrinkWrap: true,
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(
+              horizontal: getProportionateScreenWidth(7),
+              vertical: getProportionateScreenHeight(7),
             ),
-          ],
-        ),
+            decoration: BoxDecoration(
+              color: Color(0xFFF2F2F2),
+            ),
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(getProportionateScreenHeight(0)),
+                  child: WeatherContainer(context: context, model: widget.model!),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(getProportionateScreenHeight(5)),
+                  child: SavingsContainer(context: context, model: widget.model!),
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.all(getProportionateScreenHeight(5)),
+                        child: DarkContainer(
+                          itsOn: widget.model!.isLightOn ,
+                          switchButton: () {
+                            widget.model!.lightSwitch();
+                            widget.model!.isLightOn?
+                            model!.getValue(value :'on'):
+                            model!.getValue(value: 'off');
+                            Navigator.of(context).pushNamed(SmartLight.routeName);
+                          },
+                          onTap: widget.model!.lightSwitch,
+                          iconAsset: 'assets/icons/svg/light.svg',
+                          device: 'Lightening',
+                          deviceCount: '1 lamp',
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.all(getProportionateScreenHeight(5)),
+                        child: DarkContainer(
+                          itsOn: widget.model!.isACON,
+                          switchButton: widget.model!.acSwitch,
+                          onTap: () {
+                            Navigator.of(context).pushNamed(SmartAC.routeName);
+                          },
+                          iconAsset: 'assets/icons/svg/ac.svg',
+                          device: 'AC',
+                          deviceCount: '4 devices',
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: EdgeInsets.all(getProportionateScreenHeight(5)),
+                  child: MusicWidget(context),
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.all(getProportionateScreenHeight(5)),
+                        child: DarkContainer(
+                          itsOn: widget.model!.isSpeakerON,
+                          switchButton: widget.model!.speakerSwitch,
+                          onTap: () {
+                            Navigator.of(context).pushNamed(SmartSpeaker.routeName);
+                          },
+                          iconAsset: 'assets/icons/svg/speaker.svg',
+                          device: 'Speaker',
+                          deviceCount: '1 device',
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.all(getProportionateScreenHeight(5)),
+                        child: DarkContainer(
+                          itsOn: widget.model!.isFanON,
+                          switchButton: widget.model!.fanSwitch,
+                          onTap: () {
+                            Navigator.of(context).pushNamed(SmartAC.routeName);
+                          },
+                          iconAsset: 'assets/icons/svg/fan.svg',
+                          device: 'Fan',
+                          deviceCount: '2 devices',
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: EdgeInsets.all(getProportionateScreenHeight(8)),
+                  child: AddNewDevice(context),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
